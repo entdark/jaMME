455b7927ee8adc78d54a8fe65e26af92230d180a
entdark <entdark@mail.ru>
2014-06-30 16:01:54 +0400

    fixed inability to play background music

--- codemp/client/snd_main.cpp
+++ codemp/client/snd_main.cpp
@@ -631,22 +631,23 @@ background music functions
 //	to use WAV music you'll have to do some tweaking below (but I've got other things to do so it'll have to wait - Ste)
 //
 void S_StartBackgroundTrack( const char *intro, const char *loop, int bCalledByCGameStart ) {
+	qboolean soundExists;
 	if ( !intro || !intro[0] )
 		return;
-
 	if ( !loop || !loop[0] ) 
 		loop = intro;
 
 	Com_DPrintf( "S_StartBackgroundTrack( %s, %s )\n", intro, loop );
 
-	s_background.playing = qtrue;
-	s_background.reload = qtrue;
-
 	Q_strncpyz( s_background.startName, intro, sizeof( s_background.startName ));
-	COM_DefaultExtension( s_background.startName, sizeof( s_background.startName ), ".wav" );
-
 	Q_strncpyz( s_background.loopName, loop, sizeof( s_background.loopName ));
-	COM_DefaultExtension( s_background.loopName, sizeof( s_background.loopName ), ".wav" );
+	/* S_FileExists sets correct extension */
+	soundExists = S_FileExists(s_background.startName);
+	soundExists = (qboolean)(S_FileExists(s_background.loopName) || soundExists);
+	if (soundExists) {
+		s_background.playing = qtrue;
+		s_background.reload = qtrue;
+	}
 }
 
 void S_StopBackgroundTrack( void ) {
