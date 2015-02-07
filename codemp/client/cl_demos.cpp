eddc1af57d8ad31e59c80d32501b2c288191a3c2
entdark <entdark@mail.ru>
2015-02-07 03:10:54 +0300

    no need to check, checked above

--- codemp/client/cl_demos.cpp
+++ codemp/client/cl_demos.cpp
@@ -777,7 +777,7 @@ static void demoPlayForwardFrame( demoPlay_t *play ) {
 			CL_Disconnect_f();
 		}
 		if (mme_demoAutoQuit->integer && !demoPrecaching) {
-			if (mme_demoAutoQuit->integer == 2 && !demoPrecaching)
+			if (mme_demoAutoQuit->integer == 2)
 				Cbuf_ExecuteText( EXEC_APPEND, "quit" );
 			CL_Disconnect_f();
 		}
