79c7cde009e3474dc71b1b084c86d9bb7614ed9b
entdark <entdark@mail.ru>
2017-06-09 04:59:28 +0500

    [android] update version

--- android/Mono/Properties/AssemblyInfo.cs
+++ android/Mono/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using Android.App;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ent")]
 [assembly: AssemblyProduct("jaMME")]
-[assembly: AssemblyCopyright("Copyright © ent 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © ent 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -26,8 +26,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
 [assembly: UsesPermission(Android.Manifest.Permission.Vibrate)]
